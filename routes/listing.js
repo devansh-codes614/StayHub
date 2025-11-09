754eab66938581dc76996deee8ecc355e507f28a
Devansh Tripathi <devansh5832@gmail.com>
2025-11-09 16:23:24 +0530

    fixed controller filename

--- routes/listing.js
+++ routes/listing.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-const listingController = require("../controllers/listing.js");
+const listingController = require("../controllers/listings.js");
 const multer = require("multer");
 
 const { storage } = require("../cloudConfig.js");
